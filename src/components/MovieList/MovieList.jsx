076fee9c72ca7466e0e89633402aa6badefc9a16
Tamira2804 <tamira2804@gmail.com>
2023-09-01 21:03:37 +0300

    location state

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
-  // if (!movies || movies.length === 0) {
-  //   return <p>No movies found.</p>;
-  // }
+  const location = useLocation();
+
   return (
     <ul className="movie-list">
       {movies.map(({ title, id }) => {
         return (
           <li key={id}>
-            <Link className="movie-item" to={`/movies/${id}`}>
+            <Link
+              className="movie-item"
+              to={`/movies/${id}`}
+              state={{ from: location }}
+            >
               {title}
             </Link>
           </li>
