f82076e9582d24976a3217cc18ecfe25d803cdee
Tamira2804 <tamira2804@gmail.com>
2023-09-01 21:04:20 +0300

    Update HttpMovies.js

--- src/services/HttpMovies.js
+++ src/services/HttpMovies.js
@@ -39,4 +39,14 @@ export const getMovieCast = movieId => {
   );
 };
 
+export const getMovieReviews = movieId => {
+  return fetch(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`).then(
+    res => {
+      if (res.status !== 200) {
+        return Promise.reject(new Error(`Oops, something went wrong...`));
+      } else return res.json();
+    }
+  );
+};
+
 // https://api.themoviedb.org/3/movie/420818/credits?api_key=e8e18ab747b38797cae45f67fe29be94
