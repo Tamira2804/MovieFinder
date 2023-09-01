c529a5de336d395cf7b65e54ac3fcb923a37b979
Tamira2804 <tamira2804@gmail.com>
2023-09-01 21:19:39 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container, Header, Link } from './SharedLayout.styled';
-import Loader from 'components/Loader/Loader';
 
 const SharedLayout = () => {
   return (
